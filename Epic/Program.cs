eccd184a65f0b81a956f2b2a5ae01b70c2ca9709
Opilane TTHK <opilane@tthk.ee>
2021-09-08 12:30:49 +0300

    add random

---
+++ Epic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Epic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EpicDiceGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace EpicDiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //mängus osaleb kaks mängijat
+            //iga mängija viskab täringut (1-6)
+            //programm kuvab nende visete tulemused ja kuvab ka, kes on võitja
+            // ehk kes viskas suuremat
+
+            Random rnd = new Random();
+
+            string playerOne = "Harry Potter";
+            string playerTwo = "Voldemort";
+
+            int HarryThrow = rnd.Next(1, 7);
+            int VoldemortThrow = rnd.Next(1, 7);
+
+            Console.WriteLine($"{playerOne} threw {HarryThrow};");
+            Console.WriteLine($"{playerTwo} threw {VoldemortThrow};");
+
+
+
+
+            if (HarryThrow > VoldemortThrow)
+            {
+                Console.WriteLine($"{playerOne} Wins!");
+            }
+            else if (HarryThrow < VoldemortThrow)
+            {
+                Console.WriteLine($"{playerTwo} Wins!");
+            }
+            else
+            {
+                Console.WriteLine("Draw! let them try again.");
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
